7e54990b809acf9d77e76f619a91b18d9de15b1a
George-Pura-Vida <jgjanousek@gmail.com>
2026-09-09 11:32:38 +0200

    Add MySQL configuration template

---
+++ config.example.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'db' => [
+        'host' => 'shareddb-l.hosting.stackcp.net',
+        'name' => 'zkouska_ix-39396327',
+        'user' => 'YOUR_DB_USER',
+        'pass' => 'YOUR_DB_PASSWORD',
+        'charset' => 'utf8mb4',
+    ],
+    'app' => [
+        'name' => 'Zkouška IX',
+        'timezone' => 'Europe/Prague',
+        'session_name' => 'ZKOUSKAIXSESSID',
+        'base_url' => 'https://zkouska.jirijanousek.cz',
+    ],
+];
